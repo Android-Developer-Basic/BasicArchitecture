df62edd7a3bc0481fa27283969a84bc427bbf323
LeonZhuk <lionzhuk@gmail.com>
2023-08-20 10:17:01 +0300

    DZ Ddata правка

--- app/src/main/java/ru/otus/basicarchitecture/DaDataService.kt
+++ app/src/main/java/ru/otus/basicarchitecture/DaDataService.kt
@@ -1,17 +1,12 @@
 package ru.otus.basicarchitecture
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
+
 interface DaDataService {
-    @Headers(
-        "Content-Type: application/json",
-        "Accept: application/json",
-        "Authorization: Token 33c9c6d8a38493b4c05554aa083dca49db0068fd"
-    )
     @POST("suggestions/api/4_1/rs/suggest/address")
-    fun getSuggestions(@Body request: DaDataRequest): Call<SuggestionsResponse>
+    suspend fun getSuggestions(@Body request: DaDataRequest): Response<SuggestionsResponse>
 
 }
--- app/src/main/java/ru/otus/basicarchitecture/UserInputAddressViewModel.kt
+++ app/src/main/java/ru/otus/basicarchitecture/UserInputAddressViewModel.kt
@@ -4,37 +4,48 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserInputAddressViewModel @Inject constructor(
-    private val wizardCache: WizardCache
+    private val wizardCache: WizardCache,
+    private val daDataService: DaDataService
 ) : ViewModel() {
     val validateState = MutableLiveData<ValidateState>()
     val viewState: MutableLiveData<ViewState> = MutableLiveData(ViewState())
     val suggestions = MutableLiveData<List<Suggestion>>()
 
-    fun search(query: String) {
-        MyApplication.instance.services.getSuggestions(DaDataRequest(query))
-            .enqueue(object : Callback<SuggestionsResponse> {
+    private val viewModelJob = Job()
+    private val viewModelScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-                override fun onResponse(
-                    call: Call<SuggestionsResponse>,
-                    response: Response<SuggestionsResponse>
-                ) {
+    fun search(query: String) {
+        viewModelScope.launch {
+            try {
+                val response = daDataService.getSuggestions(DaDataRequest(query))
+                if (response.isSuccessful) {
                     val data = response.body()
                     suggestions.value = data?.suggestions
-
+                    Log.d("log----------------", "onResponse: ${data?.suggestions}")
+                } else {
+                    Log.d("log----------------", response.toString())
                 }
-
-                override fun onFailure(call: Call<SuggestionsResponse>, t: Throwable) {
-                    t.printStackTrace()
-                }
-            })
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.d("log----------------", "Exception: $e")
+            }
+        }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+
 
     fun validateAndSaveAddress() {
         val stats = viewState.value!!
