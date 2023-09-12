3f3815a956b89a1bb66bdc1c452a4c12827c04e8
LeonZhuk <lionzhuk@gmail.com>
2023-09-12 09:33:48 +0300

    DZ Test правка

--- app/src/test/java/ru/otus/basicarchitecture/UserInputViewModelTest.kt
+++ app/src/test/java/ru/otus/basicarchitecture/UserInputViewModelTest.kt
@@ -3,16 +3,21 @@ package ru.otus.basicarchitecture
 import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.*
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import java.time.Instant
 
 class UserInputViewModelTest {
 
     private lateinit var viewModel: UserInputViewModel
     private lateinit var fakeWizardCache: WizardCache
+    private lateinit var mockClock: Clock
 
     @Before
     fun setUp() {
         fakeWizardCache = FakeWizardCache()
-        viewModel = UserInputViewModel(fakeWizardCache)
+        mockClock = Mockito.mock(Clock::class.java)
+        viewModel = UserInputViewModel(fakeWizardCache, mockClock)
     }
 
     @Test
@@ -29,6 +34,7 @@ class UserInputViewModelTest {
 
     @Test
     fun testIsValidFirst_notAdult() {
+        `when`(mockClock.currentMillis()).thenReturn(Instant.parse("2023-01-01T00:00:00Z").toEpochMilli())
         val notAdultDate = "10.01.2010" // Дата, когда еще нет 18 лет
         val result = viewModel.isValidFirst(notAdultDate, listOf(listOf("John", "Имя"), listOf("Doe", "Фамилия")))
         assertEquals(ValidateState.Not18, result)
@@ -36,6 +42,7 @@ class UserInputViewModelTest {
 
     @Test
     fun testIsValidFirst_valid() {
+        `when`(mockClock.currentMillis()).thenReturn(Instant.parse("2023-01-01T00:00:00Z").toEpochMilli())
         val validDate = "10.01.1990" // Валидная дата рождения
         val result = viewModel.isValidFirst(validDate, listOf(listOf("John", "Имя"), listOf("Doe", "Фамилия")))
         assertEquals(ValidateState.Ok, result)
