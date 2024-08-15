5edd533dfbe4e9eee664f0ce5694e74c9a427f52
nikolay <nazarov.nikolay15@wb.ru>
2024-08-15 21:57:51 +0300

    Сделал 2 пункт

--- app/src/main/java/ru/otus/basicarchitecture/ui/fragments/Fragment2.kt
+++ app/src/main/java/ru/otus/basicarchitecture/ui/fragments/Fragment2.kt
@@ -1,27 +1,100 @@
 package ru.otus.basicarchitecture.ui.fragments
 
+import android.os.Build
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import ru.otus.basicarchitecture.R
+import ru.otus.basicarchitecture.viewmodels.Fragment2ViewModel
 import ru.otus.basicarchitecture.viewmodels.MainViewModel
 
+@AndroidEntryPoint
 class Fragment2 : Fragment() {
+    private val viewModel: Fragment2ViewModel by viewModels()
 
-    private val viewModel: MainViewModel by viewModels()
+    private lateinit var countryEditText: EditText
+    private lateinit var cityEditText: EditText
+    private lateinit var addressEditText: EditText
+    private lateinit var nextBtn: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var toast: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_fragment2, container, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        countryEditText = view.findViewById(R.id.country)
+        cityEditText = view.findViewById(R.id.city)
+        addressEditText = view.findViewById(R.id.address)
+        nextBtn = view.findViewById(R.id.fragment2Btn)
+        toast = view.findViewById(R.id.toast)
+
+        viewModel.country.observe(viewLifecycleOwner) {
+            if (it == null || it.isBlank()) {
+                countryEditText.error = "Поле не должно быть пустым"
+            } else {
+                countryEditText.error = null
+            }
+        }
+
+        viewModel.city.observe(viewLifecycleOwner) {
+            if (it == null || it.isBlank()) {
+                cityEditText.error = "Поле не должно быть пустым"
+            } else {
+                cityEditText.error = null
+            }
+        }
+
+        viewModel.address.observe(viewLifecycleOwner) {
+            if (it == null || it.isBlank()) {
+                addressEditText.error = "Поле не должно быть пустым"
+            } else {
+                addressEditText.error = null
+            }
+        }
+
+        viewModel.isFormValid.observe(viewLifecycleOwner) { isValid ->
+            nextBtn.isEnabled = isValid
+        }
+
+        countryEditText.addTextChangedListener { viewModel.country.value = it.toString() }
+        cityEditText.addTextChangedListener { viewModel.city.value = it.toString() }
+        addressEditText.addTextChangedListener { viewModel.address.value = it.toString() }
+
+        nextBtn.setOnClickListener {
+            // Сохраняем данные из EditText в LiveData
+            viewModel.country.value = countryEditText.text.toString()
+            viewModel.city.value = cityEditText.text.toString()
+            viewModel.address.value = addressEditText.text.toString()
+
+            // Выполняем валидацию и сохраняем данные в кэш
+            if (nextBtn.isEnabled) {
+                viewModel.saveData()
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, Fragment3())
+                    .addToBackStack(null)
+                    .commit()
+                // Здесь можно перейти на следующий фрагмент или завершить процесс анкеты
+            } else {
+                Snackbar.make(toast, "Валидация не пройдена", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/otus/basicarchitecture/viewmodels/Fragment2ViewModel.kt
@@ -0,0 +1,49 @@
+package ru.otus.basicarchitecture.viewmodels
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.otus.basicarchitecture.model.Tag
+import ru.otus.basicarchitecture.repository.TagRepository
+import ru.otus.basicarchitecture.repository.WizardCache
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.util.Calendar
+import java.util.Locale
+import javax.inject.Inject
+@RequiresApi(Build.VERSION_CODES.O)
+@HiltViewModel
+class Fragment2ViewModel @Inject constructor(private val wizardCache: WizardCache) : ViewModel() {
+    val country = MutableLiveData<String>()
+    val city = MutableLiveData<String>()
+    val address = MutableLiveData<String>()
+
+    private val _isFormValid = MutableLiveData<Boolean>()
+    val isFormValid: LiveData<Boolean> get() = _isFormValid
+
+    init {
+        country.observeForever { validateForm() }
+        city.observeForever { validateForm() }
+        address.observeForever { validateForm() }
+    }
+
+    private fun validateForm() {
+        val isCountryValid = !country.value.isNullOrBlank()
+        val isCityValid = !city.value.isNullOrBlank()
+        val isAddressValid = !address.value.isNullOrBlank()
+        _isFormValid.value = isCountryValid && isCityValid && isAddressValid
+    }
+
+    fun saveData() {
+        wizardCache.country = country.value
+        wizardCache.city = city.value
+        wizardCache.address = address.value
+    }
+}
