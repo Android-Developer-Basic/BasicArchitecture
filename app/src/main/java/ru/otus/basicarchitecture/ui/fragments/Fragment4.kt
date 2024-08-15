62339707afb1c7c9a9a03ce36b620cb84158613b
nikolay <nazarov.nikolay15@wb.ru>
2024-08-15 23:01:29 +0300

    Сделал 4 пункт? правлю верстку

--- app/src/main/java/ru/otus/basicarchitecture/ui/fragments/Fragment4.kt
+++ app/src/main/java/ru/otus/basicarchitecture/ui/fragments/Fragment4.kt
@@ -5,52 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.viewModels
 import com.google.android.flexbox.FlexboxLayout
 import dagger.hilt.android.AndroidEntryPoint
 import ru.otus.basicarchitecture.R
-import ru.otus.basicarchitecture.viewmodels.Fragment3ViewModel
+import ru.otus.basicarchitecture.viewmodels.Fragment4ViewModel
 
 @AndroidEntryPoint
 class Fragment4 : Fragment() {
-    private val viewModel: Fragment3ViewModel by viewModels()
+    private val viewModel: Fragment4ViewModel by viewModels()
 
+    private lateinit var firstNameTextView: TextView
+    private lateinit var lastNameTextView: TextView
+    private lateinit var birthDateTextView: TextView
+    private lateinit var addressTextView: TextView
     private lateinit var tagFlexboxLayout: FlexboxLayout
-    private lateinit var nextBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_fragment1, container, false)
+        return inflater.inflate(R.layout.fragment_fragment4, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firstNameTextView = view.findViewById(R.id.tvFirstName)
+        lastNameTextView = view.findViewById(R.id.tvLastName)
+        birthDateTextView = view.findViewById(R.id.tvBirthDate)
+        addressTextView = view.findViewById(R.id.tvAddress)
         tagFlexboxLayout = view.findViewById(R.id.tagFlexboxLayout)
-        nextBtn = view.findViewById(R.id.fragment3Btn)
 
-        viewModel.selectedInterests.observe(viewLifecycleOwner) { selectedInterests ->
-            updateTags(selectedInterests)
-        }
+        firstNameTextView.text = viewModel.firstName ?: "N/A"
+        lastNameTextView.text = viewModel.lastName ?: "N/A"
+        birthDateTextView.text = viewModel.birthDate ?: "N/A"
+        addressTextView.text = viewModel.fullAddress
 
-        nextBtn.setOnClickListener {
-            viewModel.saveSelectedInterests()
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, Fragment4())
-                .addToBackStack(null)
-                .commit()
-        }
-
-        // Добавить теги в FlexboxLayout
+        // Отобразить интересы
         viewModel.interests.forEach { interest ->
             val tagView = createTagView(interest)
-            tagView.setOnClickListener {
-                viewModel.toggleInterest(interest)
-            }
             tagFlexboxLayout.addView(tagView)
         }
     }
@@ -59,13 +54,5 @@ class Fragment4 : Fragment() {
         val tagView = LayoutInflater.from(context).inflate(R.layout.tag_item, tagFlexboxLayout, false) as TextView
         tagView.text = interest
         return tagView
-    }
-
-    private fun updateTags(selectedInterests: Set<String>) {
-        for (i in 0 until tagFlexboxLayout.childCount) {
-            val tagView = tagFlexboxLayout.getChildAt(i) as TextView
-            val interest = tagView.text.toString()
-            tagView.isSelected = selectedInterests.contains(interest)
-        }
     }
 }
